24750d8b3592fc57a2998ed549bb8f8663872f8c
ruta_olta <alya.yakupova@botkin.ai>
2020-12-29 21:31:08 +0300

    classes added

--- gui.py
+++ gui.py
@@ -1,67 +1,89 @@
 import PySimpleGUI as sg
-# from numpy.random import random
 import matplotlib
 import matplotlib.pyplot as plt
 from draw_matrix import *
 from nussinov import *
-
-# matrix = [[sg.Frame('', [[sg.I(random.randint(1,9), justification='r', size=(3,1),enable_events=True, key=(fr*3+r,fc*3+c)) for c in range(3)] for r in range(3)]) for fc in range(3)] for fr in range(3)]
-
-layout = [
-    [
-        sg.Text("RNA sequence"),
-        sg.In(size=(25, 1), enable_events=True, key="-FOLDER-")
-    ],
-    [sg.Canvas(key="-CANVAS-")],
-    [sg.Button("Left"), sg.Button("Play"), sg.Button("Right")]
-]
-
 matplotlib.use("TkAgg")
 
 
-def validate_sequence(seq):
-    pass
+class Application:
 
-window = sg.Window(
-    "Nussinov algorithm",
-    layout,
-    location=(0, 0),
-    finalize=True,
-    element_justification="center",
-    font="Helvetica 18",
-)
+    def __init__(self):
+        self.layout = self._init_layout()
+        self.window = sg.Window(
+                    "Nussinov algorithm",
+                    self.layout,
+                    location=(0, 0),
+                    finalize=True,
+                    element_justification="center",
+                    font="Helvetica 18",
+                )
+        self.fig = matplotlib.figure.Figure(figsize=(5, 5), dpi=100)
+        self.data = None
+        self.im = None
+        self.fig_agg = None
 
-n = 8
-def gen_data(seq):
-    if seq == '':
-        data = [[0 for x in range(0, n)],  # row 1
-                [0 for x in range(0, n)],  # row 2
-                [0 for x in range(0, n)],  # row 3
-                [0 for x in range(0, n)],  # row 4
-                [0 for x in range(0, n)],  # row 5
-                [0 for x in range(0, n)],  # row 6
-                [0 for x in range(0, n)],  # row 7
-                [0 for x in range(0, n)]]  # row 8
-    else:
-        data = nussinov(seq)
-    return data
+    def _init_layout(self):
+        return [
+            [
+                sg.Text("RNA sequence"),
+                sg.In(size=(25, 1), enable_events=True, key="-FOLDER-"),
+                sg.Button("OK")
+            ],
+            [sg.Canvas(key="-CANVAS-")],
+            [sg.Button("Left"), sg.Button("Play"), sg.Button("Right")]
+        ]
+
+    def validate_sequence(seq):
+        pass
+
+    def clean_fig(self):
+        if self.fig_agg:
+            delete_fig_agg(self.fig_agg, plt)
+
+    def draw_fig(self):
+        self.fig_agg = draw(self.data[self.data.frame], self.im, self.window, self.fig)
+
+    def go_right(self):
+        if self.data.frame == len(self.data.history) - 1:
+            self.data.frame = 0
+        else:
+            self.data.frame += 1
+
+    def go_left(self):
+        if self.data.frame == 0:
+            self.data.frame = len(self.data.history) - 1
+        else:
+            self.data.frame -= 1
+
+    def run_loop(self):
+        while True:
+            event, values = self.window.read()
+            if event == "Exit" or event == sg.WIN_CLOSED:
+                break
+            elif event == 'Left':
+                if not self.data: continue
+                self.clean_fig()
+                self.go_left()
+                self.draw_fig()
+            elif event == 'Right':
+                if not self.data: continue
+                self.clean_fig()
+                self.go_right()
+                self.draw_fig()
+            elif event == 'Play':
+                if not self.data: continue
+                self.draw_fig()
+            elif event == 'OK':
+                seq = values['-FOLDER-']
+                if not seq: continue
+                self.clean_fig()
+                self.data = Nussinov(seq)
+                self.im = self.fig.add_subplot(111).imshow(self.data[self.data.frame])
+                self.draw_fig()
+        self.window.close()
 
 
-fig = matplotlib.figure.Figure(figsize=(5, 5), dpi=100)
-im = fig.add_subplot(111).imshow(gen_data(''))
-# fig_agg = draw(gen_data(''), im, window, fig)
-
-
-while True:
-    event, values = window.read()
-    if event == "Exit" or event == sg.WIN_CLOSED:
-        break
-    elif event == 'Right':
-        if exists('fig_agg'):
-            delete_fig_agg(fig_agg, plt)
-        data = gen_data(values['-FOLDER-'])
-        fig_agg = draw(data, im, window, fig)
-    elif event == 'Play':
-        data = gen_data(values[0])
-        fig_agg = draw(data, im, window, fig)
-window.close()
+if __name__ == '__main__':
+    app = Application()
+    app.run_loop()
--- nussinov.py
+++ nussinov.py
@@ -1,3 +1,6 @@
+from copy import deepcopy
+
+
 def ispair(nucl1, nucl2):
     pairs = {"A": "U", "U": "A", "G": "C", "C": "G"}
     if pairs[nucl1] == nucl2:
@@ -5,18 +8,30 @@ def ispair(nucl1, nucl2):
     return 0
 
 
-def nussinov(seq):
-    n = len(seq)
-    matrix = [[0] * n for _ in range(n)]
+class Nussinov:
+    def __init__(self, seq):
+        self.sequence = seq
+        self.history = list()
+        self.frame = 0
+        self._run()
 
-    for d in range(2, n):
-        for i in range(0, n - d):
-            j = i + d
-            arr = []
-            arr.append(matrix[i + 1][j - 1] + ispair(seq[i], seq[j]))
-            arr.append(matrix[i + 1][j])
-            arr.append(matrix[i][j - 1])
-            for k in range(i + 1, j - 1):
-                arr.append(matrix[i][k] + matrix[k + 1][j])
-            matrix[i][j] = max(arr)
-    return matrix #[0][len(seq) - 1] or 0
+    def __getitem__(self, item):
+        return self.history[item]
+
+
+    def _run(self):
+        n = len(self.sequence)
+        matrix = [[0] * n for _ in range(n)]
+
+        for d in range(2, n):
+            for i in range(0, n - d):
+                j = i + d
+                arr = []
+                arr.append(matrix[i + 1][j - 1] + ispair(self.sequence[i], self.sequence[j]))
+                arr.append(matrix[i + 1][j])
+                arr.append(matrix[i][j - 1])
+                for k in range(i + 1, j - 1):
+                    arr.append(matrix[i][k] + matrix[k + 1][j])
+                matrix[i][j] = max(arr)
+                self.history.append(deepcopy(matrix))
+        return matrix #[0][len(seq) - 1] or 0
